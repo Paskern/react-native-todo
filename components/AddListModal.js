4eafa35a24facc3879b4915911f7eb6e290af282
Paskern <StefanPakaski@gmail.com>
2020-07-13 20:09:45 +0200

    Initial

---
+++ components/AddListModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "../Colors";
+
+export default class AddListModal extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <TouchableOpacity
+          style={{ position: "absolute", top: 64, right: 32 }}
+          onPress={this.props.closeModal}
+        >
+          <AntDesign name="close" size={24} color={colors.black} />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ tempData.js
@@ -0,0 +1,74 @@
+// Temporary Data until we connect with Firebase
+
+export default tempData = [
+  {
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book flight",
+        completed: false,
+      },
+      {
+        title: "Passport check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy milk",
+        completed: true,
+      },
+      {
+        title: "Work out",
+        completed: true,
+      },
+      {
+        title: "Watch TV",
+        completed: true,
+      },
+      {
+        title: "Code this",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Party",
+    color: "#2042D9",
+    todos: [
+      {
+        title: "Buy alcohol",
+        completed: false,
+      },
+      {
+        title: "Ab workout",
+        completed: true,
+      },
+      {
+        title: "Watch TV",
+        completed: true,
+      },
+      {
+        title: "Code this",
+        completed: false,
+      },
+      {
+        title: "Code this shiet",
+        completed: false,
+      },
+    ],
+  },
+];
